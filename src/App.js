aad31b923dd569738139c7590dbe52c0f68bc017
Zaid Ansari <zaid.ansari@nonstopio.com>
2024-01-04 12:05:12 +0530

    style: Added Navigation Bar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+      <header>
+        <div className='flex justify-between text-base my-4 mx-6'>
+          <div>
+            <ul className='flex gap-4 p-2'>
+              <li>Discover</li>
+              <li>Destinations</li>
+              <li>Packages</li>
+              <li>Explore</li>
+            </ul>
+          </div>
+          <div className='flex gap-4'>
+            <button className='border-solid border-2 px-8 rounded-3xl'>
+              Join
+            </button>
+            <button className='border-solid border-2 px-8 rounded-3xl'>
+              Login
+            </button>
+          </div>
+        </div>
       </header>
+      <hr />
+      <body>
+
+      </body>
     </div>
   );
 }
